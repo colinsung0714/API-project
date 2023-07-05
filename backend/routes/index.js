577cf27f40de2845c41e0227695ed543d069924e
Colin Sung <colinsung0714@gmail.com>
2023-07-05 13:22:11 -0700

    Add an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const apiRouter = require('./api');
 
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
@@ -10,5 +10,6 @@ router.get("/api/csrf/restore", (req, res) => {
   });
 });
 
+router.use('/api', apiRouter);
 
 module.exports = router;
