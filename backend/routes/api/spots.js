5fda7dd79c3e9cb5cb885e1e8213dd58391c3d11
Colin Sung <colinsung0714@gmail.com>
2023-07-08 00:30:38 -0700

    GET Spots of Current Users

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,6 +1,39 @@
 const express = require('express')
 const router = express.Router();
 const { Spot, sequelize, Review, SpotImage } = require('../../db/models')
+const { requireAuth } =require('../../utils/auth')
+
+router.get('/current',requireAuth,async (req, res)=>{
+    const allSpots = await Spot.findAll({
+        where: {
+            ownerId: req.user.id
+        }
+    })
+    const Spots = [];
+    for(let spot of allSpots) {
+        let avgRating = await spot.getReviews({
+            attributes:[[sequelize.fn('AVG', sequelize.col('stars')), 'avgRating']]
+        })
+        let previewImage = await spot.getSpotImages({
+            attributes:['url']
+        })
+        spot = spot.toJSON();
+        for(let num of avgRating) {
+            num = num.toJSON();
+            spot.avgRating = Number.parseFloat((Object.values(num)[0])).toFixed(1);
+        }
+        for (let url of previewImage) {
+            url = url.toJSON();
+            spot.previewImage = Object.values(url)[0];
+        }
+        Spots.push(spot);
+    }
+    
+  
+    res.json({Spots})
+
+})
+
 router.get('/', async (req, res)=>{
     const allSpots = await Spot.findAll({})
     const Spots = [];
@@ -14,7 +47,7 @@ router.get('/', async (req, res)=>{
         spot = spot.toJSON();
         for(let num of avgRating) {
             num = num.toJSON();
-            spot.avgRating = Object.values(num)[0];
+            spot.avgRating = Number.parseFloat((Object.values(num)[0])).toFixed(1);
         }
         for (let url of previewImage) {
             url = url.toJSON();
