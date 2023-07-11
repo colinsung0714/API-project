ea79bb8fca615aa96be250f5e08fd9bfcf38c7b5
Colin Sung <colinsung0714@gmail.com>
2023-07-11 10:15:04 -0700

    add an Image to a Review based on the Review's id

--- backend/db/models/reviewimage.js
+++ backend/db/models/reviewimage.js
@@ -20,7 +20,6 @@ module.exports = (sequelize, DataTypes) => {
   ReviewImage.init({
     reviewId:{
       type:DataTypes.INTEGER,
-      allowNull:false
     },
     url:{
       type: DataTypes.STRING,
